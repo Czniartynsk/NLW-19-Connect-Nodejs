4c2d7ce3c22d66bd734f11c1065b3ee7fb23b3fb
Czniartynsk <bruno.as72@gmail.com>
2025-11-30 18:58:10 -0300

    Criação e configuração inicial do projeto

---
+++ src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,35 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+    app.post(
+        '/subscriptions',
+        {
+            schema: {
+                summary: 'Subscribes someone to the event',
+                tags: ['subscription'],
+                description: 'description for subscribes routes',
+                body: z.object({
+                    name: z.string(),
+                    email: z.email(),
+                }),
+                response: {
+                    201: z.object({
+                        name: z.string(),
+                        email: z.email(),
+                    }),
+                },
+            },
+        },
+        async (request, reply) => {
+            const { name, email } = request.body
+
+            // criação da inscrição do banco de dados
+
+            return reply.status(201).send({
+                name,
+                email,
+            })
+        }
+    )
+}
